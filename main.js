17b6653166a8c11ff789cc312741e648eab06c10
KodiBear <paulpellom@gmail.com>
2019-01-21 13:26:09 -0500

    afternoon break

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ $('#phoneIcon').mouseenter(function(){
 $('#phoneIcon').mouseleave(function(){
     $('#popUpImg').animate({
         height: "0vh",
-    });
-
+    }, 100)
 $('#menuIcon').click(function(){
     $('#navBarSectionLinks').height("5vh");
     $('#navBarSectionLinks').width("100vw");
-});
+})
 $('body').hover(function(){
-    $('#navBarSectionLinks').height("0vh");
-    $('#navBarSectionLinks').width("0vw");
+    $('#navBarSectionLinks').animate({
+        height:"0vh"
+        
+    }, 0.20) 
 })
 
 })
