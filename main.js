604df06fca424e0c03acf9b631bcacf7c857bfa0
KodiBear <paulpellom@gmail.com>
2019-01-28 09:07:47 -0500

    morn

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-$(document).ready(function() {
-$('#phoneIcon').mouseenter(function(){
-    $('#popUpImg').height('15vh');
-    $('#popUpImg').width('30vh');
-});
-$('#phoneIcon').mouseleave(function(){
-    $('#popUpImg').animate({
-        height: "0vh",
-    }, 100)
-$('#menuIcon').click(function(){
-    $('#navBarSectionLinks').height("5vh");
-    $('#navBarSectionLinks').width("100vw");
-})
-$('body').hover(function(){
-    $('#navBarSectionLinks').animate({
-        height:"0vh"
-        
-    }, 0.20) 
-})
+$(document).ready(function () {
 
-})
+    $('.slideshow').cycle({
+        fx: 'fade'
+    });
+
+
+    $('#phoneIcon').mouseenter(function () {
+        $('#popUpImg').height('15vh');
+        $('#popUpImg').width('30vh');
+    });
+
+    $('#phoneIcon').mouseleave(function () {
+        $('#popUpImg').animate({
+            height: "0vh",
+        }, 100)
+        $('#menuIcon').click(function () {
+            $('#navBarSectionLinks').height("5vh");
+            $('#navBarSectionLinks').width("100vw");
+        })
+        $('body').hover(function () {
+            $('#navBarSectionLinks').animate({
+                height: "0vh"
+
+            }, 0.20)
+        })
+
+    })
+
 });
- //) $('#phoneNumberPopUp').height('0vh');
-    // $('#phoneNumberPopUp').width('0vw');
