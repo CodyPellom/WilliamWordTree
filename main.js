4cfa4f7afcdd1b329376239a89ce5c7b36948e73
KodiBear <paulpellom@gmail.com>
2019-02-20 08:08:18 -0500

    lulz

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
 
-
-        $('#menuIcon').click(function () {
-            $('#navBarSectionLinks').height("5vh");
-            $('#navBarSectionLinks').width("100vw");
-        })
-        $('body').hover(function () {
-            $('#navBarSectionLinks').animate({
-                height: "0vh"
-
-            }, 0.20)
-        })
+    var transEffect = Barba.BaseTransition.extend({
+        start: function () {
+            this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
+        },
+        fadeInNewcontent: function (nc) {
+            nc.hide();
+            var _this = this;
+            $(this.oldContainer).fadeOut(1000).promise().done(() => {
+                nc.css('visibility', 'visible');
+                nc.fadeIn(1000, function () {
+                    _this.done();
+                })
+            });
+        }
+    });
+    Barba.Pjax.getTransition = function () {
+        return transEffect;
+    }
+    Barba.Pjax.start()
 
 
 
